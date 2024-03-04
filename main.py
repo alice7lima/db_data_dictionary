c4765e8e070daf4424fd6277fbdb1fa25f751ade
alice7lima <alice7252011@gmail.com>
2024-03-03 22:35:04 -0300

    Ajustes na main.

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ try:
     opts, args = getopt.getopt(argv, "d:s:",["sgbd=", "schema="]) 
     opts = dict(opts)
 
-    builder = PostgresDict(schema=opts['--schema'])
-    dicionario = builder.pg_data_dictionary()
+    if opts['--sgbd'] == 'postgres':
+        builder = PostgresDict(schema=opts['--schema'])
+        dicionario = builder.pg_data_dictionary()
 
-    dicionario.to_excel('test_output.xlsx', index=False)
+        dicionario.to_excel('test_output.xlsx', index=False)
+
+    else:
+        print("Ainda não há implementação para este SGBD. Por favor aguarde.")
+
 except: 
     print("Parâmetros inválidos. Informe o sgbd e o schema desejados. Ex.: --sgbd postgres --schema vendas") 
